f8f3cf1c08fe0276a7b9328c41341c6bb00ca59e
maynul <mamun.harf@gmail.com>
2023-05-07 13:08:54 +0600

    Full page scrolling

--- visite.js
+++ visite.js
@@ -3,8 +3,29 @@ import { Selector } from 'testcafe';
 fixture`TestController.scrollIntoView`
     .page`https://erpgap.devxhub.com//`;
 
-test('Scroll the webpage', async t => {
+/* test('Scroll the webpage', async t => {
+    await t.scrollBy(0,300);
+    await t.wait(500);
+
+    await t.scrollBy(0,300);
+    await t.wait(500);
+
+    await t.scrollBy(0,300);
+    await t.wait(500);
+
+    await t.scrollBy(0,300);
+    await t.wait(500);
+
+    await t.scrollBy(0,300);
+    await t.wait(500);
+
     await t.scrollBy(0,300);
     await t.wait(500);
     
+}); */
+test('Scroll the webpage', async t => {
+    for (let i = 0; i < 20; i++) {
+        await t.scrollBy(0,300);
+        await t.wait(500);
+    }
 });
