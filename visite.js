1062b52b87fa0d1001c03d0c74ac8b280d104451
maynul <mamun.harf@gmail.com>
2023-05-07 15:54:16 +0600

    all scrolling

--- visite.js
+++ visite.js
@@ -3,30 +3,14 @@ import { Selector } from 'testcafe';
 fixture`TestController.scrollIntoView`
     .page`https://erpgap.devxhub.com//`;
 
-/* test('Scroll the webpage', async t => {
-    await t.scrollBy(0,300);
-    await t.wait(500);
-
-    await t.scrollBy(0,300);
-    await t.wait(500);
-
-    await t.scrollBy(0,300);
-    await t.wait(500);
-
-    await t.scrollBy(0,300);
-    await t.wait(500);
-
-    await t.scrollBy(0,300);
-    await t.wait(500);
-
-    await t.scrollBy(0,300);
-    await t.wait(500);
-    
-}); */
-
 test('Scroll the webpage', async t => {
     for (let i = 0; i < 20; i++) {
         await t.scrollBy(0,300);
+        await t.wait(500);
+    }
+
+    for (let i = 0; i < 20; i++) {
+        await t.scrollBy(0,-300);
         await t.wait(500);
     }
 });
